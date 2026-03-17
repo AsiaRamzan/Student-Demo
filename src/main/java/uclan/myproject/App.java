5e211d18e4386fffb36081a0b0d160bd103693aa
ARamzan6 <ARamzan6@U220972.ntds.uclan.ac.uk>
2026-03-17 14:58:00 +0000

    CI/CD demo

---
+++ src/main/java/uclan/myproject/App.java
@@ -0,0 +1,17 @@
+package uclan.myproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello World!" );
+//    }
+    
+    public static String greet() {
+        return "Hello CI/CD";
+    }
+}
---
+++ src/test/java/uclan/myproject/AppTest.java
@@ -0,0 +1,16 @@
+package uclan.myproject;
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+public class AppTest {
+	  @Test
+	    void testGreeting() {
+	        assertEquals("Hello CI/CD", App.greet());
+	    }
+
+    
+}
